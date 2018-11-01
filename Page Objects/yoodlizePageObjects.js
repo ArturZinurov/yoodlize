09e07c24d15d6f7887be6fdce195be69d871aea8
Artur Zinurov <arturzinurov971015@gmail.com>
2018-11-01 14:58:23 -0600

    yoodlize page object

---
+++ Page Objects/yoodlizePageObjects.js
@@ -0,0 +1,89 @@
+module.exports = {
+    url: 'https://alpha.yoodlize.com/',
+    elements: {
+        // Home Page
+        homePage: 'div[class="navbar-header"]',
+        seeAll: '.sc-dNLxif iUxiiF sc-gqjmRU fdocQD',
+        objectSlider: 'div[class="slider sc-btzYZH lgYiAd sc-bwzfXH loXcNn sc-bdVaJa iHZvIS"]',
+        //circle things
+        businessEquipment: 'div[data-reactid="629"]',
+        electronics: 'div[data-reactid="634"]',
+        recreationalVehicles: 'div[data-reactid="639"]',
+        fashion: 'div[data-reactid="645"]',
+        homeAndGarden: 'div[data-reactid="650"]',
+        outdoorGear: 'div[data-reactid="655"]',
+        partyEquipment: 'div[data-reactid="661"]',
+        properties: 'div[data-reactid="666"]',
+        experiences: 'div[data-reactid="671"]',
+        sportingEquipment: 'div[data-reactid="677"]',
+        tools: 'div[data-reactid="682"]',
+        toysAndGames: 'div[data-reactid="687"]',
+        // Search
+        searchField: 'input[type="text"]',
+        searchBtn: 'button[class="sc-kPVwWT bwXMGY sc-ifAKCX kzsifC"]',
+        // List Products
+        listProduct: 'span[data-reactid="34"]',
+        quideMe: '//button[text()="GUIDE ME"]',
+        iGotIt: '//button[text()="I GOT IT"]',
+        // Listig of an item
+        title: 'input[type="text"]',
+        description: 'textarea[class="sc-gHboQg bQZZfx"]',
+        fashionPick: '//option[text()="Fashion"]',
+        businessEquipmentPick: '//option[text()="Business Equipment"]',
+        recreationalVehiclesPick: '//option[text()="Recreational Vehicles"]',
+        toolsPick: '//option[text()="Tools"]',
+        electronicsPick: '//option[text()="Electronics"]',
+        partyEquipmentPick: '//option[text()="Party Equipment"]',
+        homeAndGardenPick: '//option[text()="Home and Garden"]',
+        outdoorGearPick: '//option[text()="Outdoor Gear"]',
+        sportingEquipmentPick: '//option[text()="Sporting Equipment"]',
+        propertiesPick: '//option[text()="Properties"]',
+        experiencesPick: '//option[text()="Experiences"]',
+        toysAndGamesPick: '//option[text()="Toys and Games"]',
+        nextBtn: 'button[class="sc-kPVwWT fqLuIb sc-ifAKCX kzsifC"]',
+        streetAddres: 'input[name="street"]',
+        city: 'input[name="city"]',
+        state: 'input[name="state"]',
+        zipcode: 'input[name="zipcode"]',
+        dayPickee: 'div[class="DayPicker-Day"]',
+        dailyRate: 'input[type="dailyRate"]',
+        rules: 'input[class="_3wXqb',
+        addBtn: '//button[text()="ADD"]',
+        // Profile
+        avatarIcon: 'a[role="button"]',
+        dashboard: 'div[data-reactid="56"]',
+        editProfile: 'div[data-reactid="60"]',
+        accountSetting: 'div[data-reactid="64"]',
+        logout: 'div[data-reactid="69"]',
+        inbox: '//div[text()="Inbox"]',
+        listing: '//div[text()="Listing"]',
+        reviews: '//div[text()="Reviews"]',
+        profile: '//div[text()="Profile"]',
+        account: '//div[text()="Account"]',
+        //Edit Profile
+        firsrName: 'input[name="firstName"]',
+        lastName: 'input[name="lastName"]',
+        gender: '//option[text()="Gender"]',
+        malePick: '//option[text()="Male"]',
+        femalePick: '//option[text()="Female"]',
+        otherPick: '//option[text()="Other"]',
+        month: '//option[text()="5"]',
+        day: '//option[text()="30"]',
+        year: '//option[text()="1985"]',
+        emailAddress: 'input[name="email"]',
+        language: '//option[text()="English"]',
+        currency: '//option[text()="USD"]',
+        location: 'input[name="location"]',
+        describeY: 'textarea[name="info"]',
+        
+
+
+
+
+    
+
+
+
+
+    }
+}
---
+++ nightwatch.props.js
@@ -0,0 +1,5 @@
+module.exports = {
+    resourcePath: "C:\\Users\\Arthur\\Desktop\\QaDevMountain\\Selenium\\",
+    seleniumServer: "selenium-server-standalone-3.14.0.jar",
+    chromedriver: "chromedriver.exe"
+ }
