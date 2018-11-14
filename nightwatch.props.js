9f4d32d60f23c085b8beea6807b2d19165915f8a
Madeline Thomas <madelinethomas@Madelines-MacBook-Air.local>
2018-11-14 11:55:31 -0700

    fixed untracked files

--- nightwatch.props.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    resourcePath: "C:\\Users\\Arthur\\Desktop\\QaDevMountain\\Selenium\\",
-    seleniumServer: "selenium-server-standalone-3.14.0.jar",
-    chromedriver: "chromedriver.exe"
-}
